70a8cb62ceb9730ab28f9c6d36d1e3bd0b3ed0b8
shitalsadgir <sheetalsadgir@gmail.com>
2022-04-28 14:12:08 -0500

    initial commit

---
+++ src/main/java/com/springbot/transaction/springbooot_transaction/SpringboootTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.springbot.transaction.springbooot_transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboootTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboootTransactionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springbot/transaction/springbooot_transaction/SpringboootTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbot.transaction.springbooot_transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboootTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
